bdbd1b2dfc7404b0a1db99162f4b9267772511b1
Sirotko Maksim <sirotko.2015@mail.ru>
2025-11-26 14:49:53 +0300

    feature: added file type validation, docstrings. Removed unnecessary code.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import csv
-from tabulate import tabulate # type: ignore
+import os
+from tabulate import tabulate
 from collections import defaultdict
 from typing import List, Dict, Any, Callable
 
@@ -12,7 +13,9 @@ def main() -> None:
     parser.add_argument(
         '--files',
         nargs='+',
+        metavar='FILE',
         required=True,
+        type=validate_file_extension,
         help="Пути к одному или нескольким CSV-файлам с данными сотрудников"
     )
     parser.add_argument(
@@ -38,8 +41,16 @@ def main() -> None:
 
     print_report(args.report, report_data)
 
+def validate_file_extension(filepath):
+    """ Проверяет расширение файла """
+
+    _, extension = os.path.splitext(filepath)
+    if extension.lower() not in ['.csv']:
+        raise argparse.ArgumentTypeError("Поддерживаются только файлы с расширением .csv")
+    return filepath
+
 def read_employees_from_files(filenames: List[str]) -> List[Dict[str, str]]:
-    """Читает все CSV-файлы и возвращает список словарей-сотрудников."""
+    """ Читает все CSV-файлы и возвращает список словарей-сотрудников """
     employees = []
     for filename in filenames:
         with open(filename, mode='r', encoding='utf-8', newline='') as f:
@@ -49,19 +60,21 @@ def read_employees_from_files(filenames: List[str]) -> List[Dict[str, str]]:
                 
             for row in reader:
                 cleaned_row = {key.strip(): value.strip() for key, value in row.items()}
-
+                
                 employees.append(cleaned_row)
+
     return employees
 
 
 def performance_report(employees: List[Dict[str, str]]) -> List[Dict[str, Any]]:
-    """Отчёт: средняя эффективность по позициям."""
+    """ Отчёт: средняя эффективность по позициям """
+    # Группируем позиции и все значения эффективности сотрудников
     position_stats = defaultdict(list)
     for employee in employees:
         try:
             perf = float(employee['performance'])
             position = employee['position']
-            position_stats[position].append(perf)
+            position_stats[position].append(perf) 
         except ValueError:
             continue  # пропускаем некорректные значения
 
@@ -72,30 +85,22 @@ def performance_report(employees: List[Dict[str, str]]) -> List[Dict[str, Any]]:
             'Position': position,
             'Avg Performance': round(avg_perf, 2)
         })
-
+    
     # Сортировка по убыванию средней эффективности
     return sorted(result, key=lambda x: x['Avg Performance'], reverse=True)
 
 
 # Для возможного расширения
 REPORTS: Dict[str, Callable] = {
-    'performance': performance_report,
-
+    'performance': performance_report
 }
 
 def print_report(report_name: str, data: List[Dict[str, Any]]) -> None:
-    
-    if not data:
-        print("Нет данных для отчёта.")
-        return
-
+    """ Выводит таблицу с отчётом """
     headers = data[0].keys()
-    print(headers)
-    rows = [[row[h] for h in headers] for row in data]
+    rows = [[i+1,*[row[h] for h in headers]] for i,row in enumerate(data)] # Форматирование словарей для вывода
     print(f"Отчёт: {report_name}")
     print(tabulate(rows,headers=list(headers), floatfmt=".2f"))
-
-
 
 if __name__ == '__main__':
     main()
